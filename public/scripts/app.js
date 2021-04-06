9f3495caca8500e630b12d129d6eab44bd1d1825
Ivan Paponin <eldkirk@gmail.com>
2021-04-06 20:59:20 +0300

    rel 5: Preview блок - отладка.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,52 @@
 const uploadForm = document.forms.uploadForm;
 const userImagesDiv = document.querySelector('#user-images');
+const filesInput = document.querySelector('#files-input');
+const preview = document.createElement('div');
+
+preview.classList.add('preview');
+
+filesInput.parentElement.insertAdjacentElement('afterend', preview);
+
+filesInput.addEventListener('change', (event) => {
+  if (!event.target.files) return;
+
+  let files = Array.from(event.target.files);
+
+  files.forEach(file => {
+    preview.innerHTML = '';
+    if (!file.type.match('image')) return;
+
+    const reader = new FileReader();
+
+    reader.onload = ev => {
+      const src = ev.target.result;
+      preview.insertAdjacentHTML('afterbegin', `
+      <div class="preview-image">
+      <div class="preview-remove" data-name="${file.name}">&times;</div>
+      <img src="${src}" alt="${file.name}"/>
+      </div>
+      `)
+    };
+
+    reader.readAsDataURL(file);
+
+  });
+
+  preview.addEventListener('click', event => {
+    if (!event.target.dataset.name) return;
+
+    const {name} = event.target.dataset;
+    files.filter(file => file.name !== name);
+
+    const block = preview
+    .querySelector(`[data-name="${name}"`)
+    .closest('.preview-image');
+
+    block.classList.add('removing');
+    setTimeout(() => block.remove(), 300)
+  })
+
+});
 
 // console.log(userImagesDiv);
 
